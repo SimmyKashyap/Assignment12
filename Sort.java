39e54c14176382a2dee08c389eba14fea9d874fd
SimmyKashyap <39344009+SimmyKashyap@users.noreply.github.com>
2018-12-28 23:39:59 +0530

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+import java.util.Collections;
+
+
+public class Sort {
+
+	
+public static void main(String[] args) {
+	
+ArrayList<String> s = new ArrayList<String>();
+	
+	s.add("Gunjan");
+		
+s.add("Ambika");
+		
+s.add("Ananyaa");
+		
+Collections.sort(s);
+		
+System.out.println(s);
+
+		
+ArrayList<Integer> i = new ArrayList<Integer>();
+		
+i.add(1);
+		
+i.add(22);
+		
+i.add(3);
+		
+Collections.sort(i);
+		
+System.out.println(i);
+	
+}
+
+}
